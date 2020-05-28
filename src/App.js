6cf0bde3060c0bdaee7f392ee88099cc22dfacac
gabrielaflores09 <gabiiflores09@gmail.com>
2020-05-28 01:05:11 -0300

    responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
+import logoBB from './images/logoBB.png';
 
+const Boton = styled.button`
+  background: #E3BC37;
+  font-family: 'Itim', cursive;
+  color: black;
+  margin: 1.5rem 0rem 0.5rem 1rem;
+  padding: 0.5rem;
+  font-size: 1.5rem;
+  border: 2px solid black;
+  border-radius: 0.5rem;
+  
+  :hover {
+    cursor: pointer;
+    color: white;
+    background: #EFCD32;
+  }
+  
+`;
 function App() {
+
+  const [frase, guardarFrase] = useState({});
+
+  const consultarAPI = async () =>{
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    guardarFrase(frase[0]);  
+  }
+
+  useEffect( () => {
+    consultarAPI();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+          <img src={logoBB} className="col-12 col-sm-10 col-md-10 col-lg-6"/>
+      </div>
+      <div className="container">
+      <Frase
+        frase = {frase}
+      />
+      </div>
+        <Boton
+          onClick={consultarAPI}
+        >Obtener Frase</Boton>
     </div>
+    
   );
 }
 
---
+++ src/components/Frase.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorFrase =  styled.div`
+    padding: 1.5rem 1.5rem 0.5rem 1.5rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width: 650px;
+
+    h1 {
+        font-family: 'Itim', cursive;
+        text-align: center;
+        position: relative;
+        padding: 1rem;
+        font-size: 1.5rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 6rem;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+
+        &::after {
+            content: close-quote;
+            font-size: 6rem;
+            position: absolute;
+            right: -1rem;
+        }
+        @media (max-width: 767.98px){
+            font-size: 1.3rem;
+
+            &::before,
+            &::after {
+                font-size: 4rem;
+            }
+
+        }
+    }
+
+    p{
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1rem;
+        font-weight: bold;
+        text-align: left;
+        color: #666;
+    }
+`;
+
+const Frase = ({frase}) => {
+
+    return (
+        <ContenedorFrase>
+            <h1>{frase.quote}</h1>
+            <p>{frase.author}</p>
+        </ContenedorFrase>   
+    );
+}
+ 
+export default Frase;
